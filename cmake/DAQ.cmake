b100532536f5631bbdad0acc30d0b7e6922266b1
John Freeman <jcfree@daq.fnal.gov>
2024-08-01 13:40:17 -0500

    JCF: bump C++ standard up to C++20 from C++17

--- cmake/DAQ.cmake
+++ cmake/DAQ.cmake
@@ -70,7 +70,7 @@ endfunction()
 
 macro(daq_setup_environment)
 
-  set(CMAKE_CXX_STANDARD 17)
+  set(CMAKE_CXX_STANDARD 20)
   set(CMAKE_CXX_EXTENSIONS OFF)
   set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
